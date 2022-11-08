80e05ecd1274ddd8cb4753b316b84f5a4e27fd89
Igor Guimarães <igorgui.tec@gmail.com>
2022-11-08 15:24:21 -0300

    React-Router Implementado

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "./components/GlobalStyle";
+import Home from "./pages/Home";
+
+export default function App() {
+
+
+
+    return (
+        <>
+        <GlobalStyle />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+            </Routes>
+        </BrowserRouter>
+        </>
+    )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import perfil from "../images/perfil.jpg"
+
+export default function Home() {
+
+
+    return (
+        <main>
+            <img src={perfil} alt="Foto de Igor Guimarães" />
+            <h1>Igor Guimarães</h1>
+            <h2>Junior Front-End Developer</h2>
+        </main>
+    )
+}
